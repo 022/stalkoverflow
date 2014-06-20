4d5c953019c6aa365deee224c2a269347d04867a
022 <ashok.lella@gmail.com>
2014-06-20 19:32:36 +0000

    Making logs optional Hiding passwords when prompted

--- stalkoverflow.py
+++ stalkoverflow.py
@@ -8,25 +8,10 @@ import sleekxmpp
 import signal
 import pickle
 import tempfile
+import getpass
 
 from optparse import OptionParser
 from datetime import datetime
-
-def authenticate():
-    temp_file = "stalkoverflow.pkl"
-    temp_file_path =  os.path.join(tempfile.gettempdir(), temp_file)
-    if os.path.exists(temp_file_path):
-        authority = pickle.load(open(temp_file_path))
-    else:
-        print "Requesting info for initial setup:"
-        authority = {}
-        authority["to"] = raw_input("send notifications to (self@gmail.com) ")
-        authority["sender_id"] = raw_input("ID used for sending notifications (id@gmail.com) ")
-        authority["password"] = raw_input("password (for {0}) ".format(authority["sender_id"]))
-        pickle.dump(authority, open(temp_file_path,"wb"))
-    return authority
-
-
 
 class SendMsgBot(sleekxmpp.ClientXMPP):
     def __init__(self, sender_id, password, recipient, message):
@@ -54,12 +39,34 @@ def send_chat(message, authority):
         print("Unable to connect.")
 
 def setup_logging():
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
     logging.basicConfig(filename='logs/stalkLog{0}.txt'.format(datetime.now().strftime('%Y%m%d_%H_%M_%S')), format="[%(asctime)s][%(levelname)s] %(message)s", datefmt="%Y%m%d %H:%M:%S", level=logging.INFO)
 
 def signal_handling(signum, frame):
     logging.info("Exiting")
     sys.exit()
     
+def get_option_parser():
+    options = OptionParser(description=__doc__)
+    options.add_option('--auth', action="store_true", dest='reauth', default=False, help="Re run authenticative process")
+    options.add_option('--logs', action="store_true", dest='logs_on', default=False, help="Log output to disk")
+    return options.parse_args()
+                    
+def authenticate(reauth):
+    temp_file = "stalkoverflow.pkl"
+    temp_file_path =  os.path.join(tempfile.gettempdir(), temp_file)
+    if os.path.exists(temp_file_path) and not reauth:
+        authority = pickle.load(open(temp_file_path))
+    else:
+        print "Requesting info for initial setup:"
+        authority = {}
+        authority["to"] = raw_input("send notifications to (self@gmail.com) ")
+        authority["sender_id"] = raw_input("ID used for sending notifications (id@gmail.com) ")
+        authority["password"] = getpass.getpass("password (for {0}) ".format(authority["sender_id"]))
+        pickle.dump(authority, open(temp_file_path,"wb"))
+    return authority
+
 def parseFeed(rss_url):
     feed=feedparser.parse(rss_url)
     new_title=feed["entries"][0]["title"]
@@ -85,8 +92,10 @@ def stalk(tag, authority):
         
 def main():
     signal.signal(signal.SIGINT, signal_handling)
-    authority = authenticate()
-    setup_logging()
+    opts, args =  get_option_parser()
+    authority = authenticate(opts.reauth)
+    if opts.logs_on:
+        setup_logging()
     tag = raw_input("Enter a tag to stalk on: ")
     send_chat("Started stalking {0} on StalkOverflow B) ".format(tag), authority)
     logging.info("Tag = ".format(tag))
